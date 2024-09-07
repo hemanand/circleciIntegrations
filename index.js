6b8559e0ccb7f6eb6ffada5e63f5d946518212dc
hemanand <hemanand5303@gmail.com>
2024-09-08 00:37:58 +0530

    index file changed

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('My first Successs full NodeJs app Deployed!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  res.end('Circle CI Deployment Successfully!!!!!!!!!!!!!!!!!!!!!1');
 }).listen(3000);
